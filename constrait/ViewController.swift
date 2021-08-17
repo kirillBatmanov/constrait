7abe44bfa57f3bcc4ede738738b825d1b1f8a3d5
Kirill Batmanov <BatmanovKS@icloud.com>
2021-08-17 18:22:37 +0200

    Initial Commit

---
+++ constrait/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  constrait
+//
+//  Created by Кирилл Батманов on 17.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
